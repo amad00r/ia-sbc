b3a7130f8b3ddd9da3d79e1f883ddad61a5fa8e3
Amadeu Moya <amadeu.moyasarda@bsc.es>
2025-04-29 20:10:57 +0200

    Add json2owl script

---
+++ json2owl.py
@@ -0,0 +1,15 @@
+# print("@prefix : <http://example.org/ricorico#> .")
+# print("@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .")
+# print("@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .")
+# print("@base <http://example.org/ricorico> .")
+# print('<http://example.org/ricorico> rdf:type owl:Ontology ; rdfs:comment "ontologia ricorico amadeu pol juan" .')
+
+import unicodedata
+import json
+import re
+
+with open("ricorico.json", "r") as f:
+	for obj in json.load(f):
+		if obj["type"] in ("Origen", "Temporada", "Preparacion"):
+			ident = re.sub(r'\s+', '_', unicodedata.normalize("NFKD", obj["id"]).encode("ASCII", "ignore").decode("utf-8").lower())
+			print(f':{ident} rdf:type owl:NamedIndividual , :{obj["type"]} ; :nombre "{obj["id"]}"')
