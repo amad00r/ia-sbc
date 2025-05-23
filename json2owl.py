0031772166460cea81dba8e8a995f67f0dd09176
Zaireck <juannblue12@gmail.com>
2025-05-23 16:16:49 +0200

    Modify json2owl script and add test instances in json

--- json2owl.py
+++ json2owl.py
@@ -11,7 +11,7 @@ import re
 def format_transformation(i):
 	return re.sub(r'\s+', '_', unicodedata.normalize("NFKD", i).encode("ASCII", "ignore").decode("utf-8").lower())
 
-with open("ricorico-instances.json", "r", encoding="utf-8") as f:
+with open("ricorico-instances-test.json", "r", encoding="utf-8") as f:
 	for obj in json.load(f):
 		ident = format_transformation(obj["id"])
 		print(f':{ident} rdf:type owl:NamedIndividual ,\n\t\t\t:{obj["type"]} ;')
@@ -23,13 +23,13 @@ with open("ricorico-instances.json", "r", encoding="utf-8") as f:
 			print(f'\t\t:lactosaFree "{str(obj["lactosaFree"]).lower()}"^^xsd:boolean ;')
 			print(f'\t\t:nombre "{ident}" ;')
 			print(f'\t\t:precio "{obj["precio"]}"^^xsd:float .')
-		elif obj["type"] == "Ingrediente":
-			print('\t\t:disponibleEn ' + " , ".join(f":{str(format_transformation(e))}" for e in obj["idsTemporadas"]) + ' ;')	           
+		elif obj["type"] == "Ingrediente":	           
 			print(f'\t\t:glutenFree "{str(obj["glutenFree"]).lower()}"^^xsd:boolean ;')
 			print(f'\t\t:lactosaFree "{str(obj["lactosaFree"]).lower()}"^^xsd:boolean ;')
 			print(f'\t\t:nombre "{ident}" .')
 		elif obj["type"] == "Plato":
 			print('\t\t:compuestoPor ' + " , ".join(f":{str(format_transformation(e))}" for e in obj["idsIngredientes"]) + ' ;')  
+			print('\t\t:disponibleEn ' + " , ".join(f":{str(format_transformation(e))}" for e in obj["idsTemporadas"]) + ' ;')
 			print(f'\t\t:esCategoria :{format_transformation(obj["idCategoria"])} ;')
 			print(f'\t\t:esPreparacion :{format_transformation(obj["idPreparacion"])} ;')
 			if obj["idsBebidasIncompatibles"]:
